1f5bceb1f97c3966d6ab80e5fe40f28913342de2
Rajendra Kumar <raj.challenge.123@gmail.com>
2015-11-08 01:52:42 -0500

    Applications.conf - configuration file consist of what log files to monitor.  - Expects Filename, Error Condition pattern and IP and Port pattern  - For testing purpose, used auth.log and auth1.log
     confFileReader.py - This utility reads the configuration file and builds the pattern dictionary
    
    IntrusionPreventionSystem
      - Added the logic to handle upto 10 works in the queue.
      - Removed the explicit locks and conditional variables.
      - Instead used Queues, which had implicit race condition handling.

--- IntrusionPreventionSystem.py
+++ IntrusionPreventionSystem.py
@@ -3,17 +3,22 @@ import sys, time
 import threading
 import subprocess
 import re
+from confFileReader import confFileReader
+from Queue import Queue
+
 # Key: IP address
 # Value: [start_time, count, block_start, force_remove]
 # force_remove: is for those IP addresses which are unblocked by the admin
 #               which is basically removing the IP Table entry
 iptable={}
+NWORKS = 10
 
 # Synchronization primitives
 lock = threading.RLock()
 fullCondition = threading.Condition(lock)
 # Work Queue
 queue = []
+wque = Queue(NWORKS)
 
 def get_ip_from_line(line, regex, ip_port_regex):
     if not line:
@@ -96,9 +101,66 @@ class Consumer(threading.Thread):
             # time.sleep(1)
         print "Consumer End..!! IP: ", ipaddr[0]
 
-class Producer(threading.Thread):
+class consumer(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
+
+    def run(self):
+        while True:
+            if not wque.empty():
+                print "Consumer came out of wait..!!!! Queue: ", wque.queue
+                ipaddr = wque.get(1) # A (IP, Port) tuple
+                print "Consuming..!! IP: ", ipaddr, " rem : ", wque.queue
+                if ipTableManager.process_new_ip(ipaddr[0]) == True:
+                    # Block the IP
+                    try:
+                        subprocess.call(["./allowBlock.sh", ipaddr[0], "DROP"])
+                        print "[Consumer] Successfully blocked: ", ipaddr[0]
+                    except:
+                        print "**** [Consumer] Blocking ", ipaddr[0], " Failed ", sys.exc_info()[0]
+                time.sleep(5)
+
+class producer(threading.Thread):
+    def __init__(self, filename, err_pat,  ipport_pat):
+        threading.Thread.__init__(self)
+        self.filename = filename
+        self.error_pattern = err_pat
+        self.ipport_pattern = ipport_pat
+
+    def run(self):
+        try:
+            fileDesc = open(self.filename, "r")
+            # fileDesc = open("./auth.log", "r")
+            fileDesc.seek(0,2)
+            while True:
+                if not wque.full():
+                    # print "Producer came out of wait..!!!!"
+                    # Get the line from the log file
+                    # Check if the authentication failed
+                    # Obtain the IP address from the line to block
+                    line = fileDesc.readline().strip()
+                    # Parameters:
+                    # param1: line from the file
+                    # param2: pattern to first match in case of failure
+                    # param3: pattern to identify the ip and port number
+                    ip = get_ip_from_line(line, self.error_pattern, self.ipport_pattern)
+                    if not ip:
+                        time.sleep(0.1)
+                        continue
+                    else:
+                        print "Producing: ", ip, " Queue ", wque.queue
+                        # Need to add more logic to get only the IP Address, Port Tuple
+                        wque.put(ip)
+                    time.sleep(1)
+        except:
+            print "File : ", self.filename, " has some issues.!"
+
+class Producer(threading.Thread):
+    def __init__(self, filename, err_pat,  ipport_pat):
+        threading.Thread.__init__(self)
+        self.filename = filename
+        self.error_pattern = err_pat
+        self.ipport_pattern = ipport_pat
 
     def run(self):
         # Read the log file
@@ -109,36 +171,40 @@ class Producer(threading.Thread):
         # So that the producer will make an entry to the table
         # fileDesc = open("/var/adm/log/auth.log", "r")
         global fullCondition, queue, lock
-        fileDesc = open("./auth.log", "r")
-        fileDesc.seek(0,2)
-        while True:
-            # print "In producer"
-            fullCondition.acquire()
-            if len(queue) >= 1:
-                # Assuming queue is full
-                print "In cond wait of producer.!"
-                fullCondition.wait()
-            # Get the line from the log file
-            # Check if the authentication failed
-            # Obtain the IP address from the line to block
-            line = fileDesc.readline().strip()
-            # Parameters:
-            # param1: line from the file
-            # param2: pattern to first match in case of failure
-            # param3: pattern to identify the ip and port number
-            ip = get_ip_from_line(line, r'sshd.*Failed', r'from\s+(\d+\.\d+\.\d+\.\d+)\s+port\s+(\d+)')
-            if not ip:
-                time.sleep(0.1)
-                continue
-            else:
-                # Need to add more logic to get only the IP Address, Port Tuple
-                queue.append(ip)
-            print "Producing: ", ip, " Queue ", queue
-            fullCondition.notify()
-            # print "Notifying the consumer.."
-            fullCondition.release()
-            # print "Released..!!!"
-            # time.sleep(5)
+        try:
+            fileDesc = open(self.filename, "r")
+            # fileDesc = open("./auth.log", "r")
+            fileDesc.seek(0,2)
+            while True:
+                # print "In producer"
+                fullCondition.acquire()
+                if len(queue) >= 1:
+                    # Assuming queue is full
+                    print "In cond wait of producer for ", self.filename
+                    fullCondition.wait()
+                # Get the line from the log file
+                # Check if the authentication failed
+                # Obtain the IP address from the line to block
+                line = fileDesc.readline().strip()
+                # Parameters:
+                # param1: line from the file
+                # param2: pattern to first match in case of failure
+                # param3: pattern to identify the ip and port number
+                ip = get_ip_from_line(line, self.error_pattern, self.ipport_pattern)
+                if not ip:
+                    time.sleep(0.1)
+                    continue
+                else:
+                    # Need to add more logic to get only the IP Address, Port Tuple
+                    queue.append(ip)
+                print "Producing: ", ip, " Queue ", queue
+                fullCondition.notifyAll()
+                # print "Notifying the consumer.."
+                fullCondition.release()
+                # print "Released..!!!"
+                time.sleep(5)
+        except:
+            print "File : ", self.filename, " has some issues.!"
 
 if __name__ == "__main__":
     print "Application Started.!"
@@ -169,8 +235,21 @@ if __name__ == "__main__":
     # t2 - consumes from the queue and builds ip tables
     # t3 - unblocks the IPs based on block timeouts and admin requests
 
-    Producer().start()
-    Consumer().start()
+    conf_file = confFileReader('config/Applications.conf')
+    conf_file.run()
+    nproducers = len(conf_file.get_patterns().keys())
+    print nproducers
+    patterns = conf_file.get_patterns()
+    for files in patterns.keys():
+        print "File ", files, " Patterns: ", patterns[files]
+        # Using Queues, implicitly has locks
+        producer(files, patterns[files][0], patterns[files][1]).start()
+
+    # Producer().start()
+    # Consumer().start()
+
+    # Using Queues, implicitly has locks
+    consumer().start()
     ublock_thread = threading.Thread(target=unblocking)
     ublock_thread.start()
     print "Application Exitted.!"
